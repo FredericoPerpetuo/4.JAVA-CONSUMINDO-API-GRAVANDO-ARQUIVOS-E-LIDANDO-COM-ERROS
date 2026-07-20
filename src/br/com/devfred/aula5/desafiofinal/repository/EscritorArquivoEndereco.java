905d67d6105d199fc01face68e22af771632c0e9
Frederico Perpetuo <fredsp.ti@gmail.com>
2026-07-20 15:24:50 -0300

    Implementação do escritor de endereços

---
+++ src/br/com/devfred/aula5/desafiofinal/repository/EscritorArquivoEndereco.java
@@ -0,0 +1,35 @@
+package br.com.devfred.aula5.desafiofinal.repository;
+
+import br.com.devfred.aula5.desafiofinal.model.Endereco;
+import br.com.devfred.aula5.desafiofinal.parser.ConversonJson;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.sql.SQLOutput;
+
+public class EscritorArquivoEndereco {
+
+    private final ConversonJson conversonJson;
+
+    public EscritorArquivoEndereco(ConversonJson conversonJson) {
+        this.conversonJson = conversonJson;
+    }
+
+    public void salvarEndereco(Endereco endereco){
+        Path diretorio = Path.of("enderecos_salvos");
+        Path arquivo = diretorio.resolve("enderecos.json");
+        try{
+            Files.createDirectories(diretorio);
+            String enderecoFormatado = conversonJson.converteEnderecoParaJsonFormatado(endereco);
+            Files.writeString(arquivo, enderecoFormatado, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+            System.out.println("Endereco gravado com sucesso em: " + arquivo.toAbsolutePath());
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+}
